e570f5fd65ba04c7026fa7e077ef469827dd37b7
Dean <dean.orenstein2@gmail.com>
2020-06-03 16:42:05 -0500

    Initial commit to add files

---
+++ dmarc.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="dmarc.aspx.cs" Inherits="Dean.dmarc" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>dmarc</title>
+    <link href="dmarcStyle.css?v=1" rel="Stylesheet" type="text/css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="functionality">
+            <div class="bigDiv">
+                <div class="halfDiv left">
+                    <asp:Button class="button" ID="getInfo" onClick="getInfo_Click" text="Deserialize an XML file" runat="server"/>
+                </div>
+                <div class="halfDiv right">
+                    <asp:Button class="button" ID="listIPs" OnClick="listIPs_Click" Text="Get bad IPs from all XML files" runat="server"/>               
+                </div>
+            </div>
+            <div class="outDiv">
+                <div class="halfDiv left out">
+                    <asp:Panel id="literal1" Visible="false" runat="server">  <!-- panel instead of div so that the properties are visible only when button is clicked -->
+                        <asp:Literal ID="output1" runat="server"></asp:Literal>
+                    </asp:Panel>
+                </div>
+                <div class="halfDiv right out">
+                    <asp:Panel id="literal2" Visible="false" runat="server">
+                        <asp:Literal ID="output2" runat="server"></asp:Literal>
+                    </asp:Panel>
+                </div>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ dmarc.aspx.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Serialization;
+using System.IO;
+using System.Net;
+
+namespace Dean
+{
+
+    // Tags for innernmost XML layer for deserialization
+    [Serializable()]
+    public class DateRange  // (used in ReportMetadata)
+    {
+        public int begin { get; set; }
+        public int end { get; set; }
+    }
+    [Serializable()]
+    public class PolicyPublished  // (used in ReportFeedback)
+    {
+        [XmlElement("domain")]
+        public string domain { get; set; }
+        [XmlElement("adkim")]
+        public string adkim { get; set; }
+        [XmlElement("aspf")]
+        public string aspf { get; set; }
+        [XmlElement("p")]
+        public string p { get; set; }
+        [XmlElement("sp")]
+        public string sp { get; set; }
+        [XmlElement("pct")]
+        public string pct { get; set; }
+    }
+    [Serializable()]
+    public class PolicyEvaluated  // (used in Row)
+    {
+        [XmlElement("disposition")]
+        public string disposition { get; set; }
+        [XmlElement("dkim")]
+        public string dkim { get; set; }
+        [XmlElement("spf")]
+        public string spf { get; set; }
+    }
+    [Serializable()]
+    public class Identifiers  // (used in Record)
+    {
+        [XmlElement("header_from")]
+        public string headerFrom { get; set; }
+    }
+    [Serializable()]
+    public class Dkim  // (used in AuthResults)
+    {
+        [XmlElement("domain")]
+        public string domain { get; set; }
+        [XmlElement("result")]
+        public string result { get; set; }
+        [XmlElement("selector")]
+        public string selector { get; set; }
+    }
+    [Serializable()]
+    public class Spf  // (used in AuthResults)
+    {
+        [XmlElement("domain")]
+        public string domain { get; set; }
+        [XmlElement("result")]
+        public string result { get; set; }
+    }
+
+    // Tags for next outter XML layer
+    [Serializable()]
+    public class ReportMetadata  // (used in ReportFeedback)
+    {
+        [XmlElement("org_name")]
+        public string orgName { get; set; }
+        [XmlElement("email")]
+        public string email { get; set; }
+        // This is not always in the report
+        [XmlElement("extra_contact_info")]
+        public string extraContactInfo { get; set; }
+        [XmlElement("report_id")]
+        public string reportID { get; set; }
+        [XmlElement("date_range")]
+        public DateRange dateRange { get; set; }
+    }
+    [Serializable()]
+    public class Row  // (used in Record)
+    {
+        [XmlElement("source_ip")]
+        public string sourceIp { get; set; }
+        [XmlElement("count")]
+        public int count { get; set; }
+        [XmlElement("policy_evaluated")]
+        public PolicyEvaluated policyEvaluated { get; set; }
+    }
+    [Serializable()]
+    public class AuthResults  // (used in Record)
+    {
+        [XmlElement("dkim", typeof(Dkim), IsNullable = true)]
+        public Dkim[] dkims { get; set; }
+        [XmlElement("spf")]
+        public Spf spf { get; set; }
+    }
+
+    // Tags for next outter XML layer
+    [Serializable()]
+    public class Record  // (used in ReportFeedback)
+    {
+        [XmlElement("row")]
+        public Row row { get; set; }
+        [XmlElement("identifiers")]
+        public Identifiers identifiers { get; set; }
+        [XmlElement("auth_results")]
+        public AuthResults authResults { get; set; }
+    }
+
+    // The outtermost XML tag (<feedback>)
+    [Serializable()]
+    [XmlRoot("ReportFeedback")]
+    public class ReportFeedback
+    {
+        [XmlElement("report_metadata")]
+        public ReportMetadata reportMetadata { get; set; }
+        [XmlElement("policy_published")]
+        public PolicyPublished policyPublished { get; set; }
+        
+        [XmlElement("record", typeof(Record))]
+        public Record[] records { get; set; }
+    }
+
+    public partial class dmarc : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+
+         // Deserialize a single XML file and output the information in a structured and readable fashion
+        protected void getInfo_Click(object sender, EventArgs e)
+        {
+            const string reportToDisplay = "report1.xml";
+
+            XmlSerializer serializer = new XmlSerializer(typeof(ReportFeedback), new XmlRootAttribute("feedback"));  // no xmls attribute for <feedback> tag, so add one
+
+            string filePath = Server.MapPath($"/xmlFiles/{reportToDisplay}");
+            Stream xmlReader = new FileStream(filePath, FileMode.Open);
+
+            ReportFeedback report;
+            report = (ReportFeedback)serializer.Deserialize(xmlReader);
+
+            // Display the properties of the report in the left hand side literal of the page
+            output1.Text = $"<h3><u>DMARC report contents for {reportToDisplay}</u>:</h3>";
+            output1.Text += "<h5>Report metadata:</h5>";
+            output1.Text += $"<p>    Org name: {report.reportMetadata.orgName}</p>";  // String interpolation makes life easier
+            output1.Text += $"<p>    Email: {report.reportMetadata.email}</p>";
+            output1.Text += $"<p>    Extra contact info: {report.reportMetadata.extraContactInfo}</p>";
+            output1.Text += $"<p>    Report ID: {report.reportMetadata.reportID}</p>";
+            output1.Text += $"<p>    Date: From {report.reportMetadata.dateRange.begin} to {report.reportMetadata.dateRange.end}</p>";
+            output1.Text += "<h5>Policy published:</h5>";
+            output1.Text += $"<p>    Domain: {report.policyPublished.domain}</p>";
+            output1.Text += $"<p>    adkim: {report.policyPublished.adkim}</p>";
+            output1.Text += $"<p>    aspf: {report.policyPublished.aspf}</p>";
+            output1.Text += $"<p>    p: {report.policyPublished.p}</p>";
+            output1.Text += $"<p>    sp: {report.policyPublished.sp}</p>";
+            output1.Text += $"<p>    pct: {report.policyPublished.pct}</p>";
+            output1.Text += "<h5>Report records:</h5>";
+            int i = 0;
+            foreach (Record r in report.records)
+            {
+                output1.Text += $"<p><b>Report {i}</b>:</p>";
+                output1.Text += $"<p>    Count: {r.row.count}</p>";
+                output1.Text += "<h6>    Policy evaluated:</h6>";
+                output1.Text += $"<p>        Disposition: {r.row.policyEvaluated.disposition}</p>";
+                output1.Text += $"<p>        dkim: {r.row.policyEvaluated.dkim}</p>";
+                output1.Text += $"<p>        spf: {r.row.policyEvaluated.spf}</p>";
+                output1.Text += $"<p>    Identifier header from: {r.identifiers.headerFrom}</p>";
+                output1.Text += "<h6>    Auth results:</h6>";
+                if (r.authResults.dkims != null)
+                {
+                    int j = 1;
+                    foreach (Dkim d in r.authResults.dkims)
+                    {
+                        output1.Text += $"<h6>        dkim {j}:</h6>";
+                        output1.Text += $"<p>            Domain: {d.domain}</p>";
+                        output1.Text += $"<p>            Result: {d.result}</p>";
+                        output1.Text += $"<p>            Selector: {d.selector}</p>";
+                        j++;
+                    }
+                }
+                output1.Text += "<h6>        spf:</h6>";
+                output1.Text += $"<p>            Domain: {r.authResults.spf.domain}</p>";
+                output1.Text += $"<p>            Result: {r.authResults.spf.result}</p>";
+                i++;
+            }
+
+            literal1.Visible = true;
+
+            xmlReader.Close();
+        }
+
+
+        // In right side Panel (literal2), display all fraudulent IP addresses in all the XML files in the directory
+        protected void listIPs_Click(object sender, EventArgs e)
+        {
+            const string dirName = "xmlFiles";
+            DirectoryInfo xmlDir = new DirectoryInfo($"C:\\Users\\Dean Orenstein\\source\\repos\\Dean\\{dirName}");
+            FileInfo[] files = xmlDir.GetFiles("*.*");
+            // Using a list of key:value pairs since theres a good chance that duplicate keys will emerge (not allowed in Dictionary)
+            List <KeyValuePair<string, string>> badIPsAndReasons = new List<KeyValuePair<string, string>>();
+
+            // For each file, first deserialize the XML to ReportFeedback object
+            int i = 1;
+            System.Diagnostics.Debug.WriteLine("Fraudulent IP addresses that are sending invalid emails from the agencysystems domain:");
+            foreach (FileInfo file in files)
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(ReportFeedback), new XmlRootAttribute("feedback"));
+                string filePath = Server.MapPath($"/{dirName}/{file.ToString()}");
+                ReportFeedback currReport;
+                Stream xmlReader = new FileStream(filePath, FileMode.Open);
+                currReport = (ReportFeedback)serializer.Deserialize(xmlReader);
+
+                // For each record in this file, get the bad IPs (<source_ip>)
+                foreach (Record record in currReport.records)
+                {
+                    string badIP = record.row.sourceIp;
+                    // See the reason for why the IP is fraud
+                    string reason = "";
+                    if (record.row.policyEvaluated.dkim == "fail")
+                    {
+                        if (record.row.policyEvaluated.spf == "fail")
+                            reason = "Failed DKIM and SPF checks";
+                        else
+                            reason = "Failed DKIM check";
+                    }
+                    else if (record.row.policyEvaluated.spf == "fail")
+                        reason = "Failed SPF check";
+                    else  // Both checks pass -> IP's identified (count) > 1
+                        reason = "Multiple IP's identified (count > 1)";
+                    // Add ip:reason pair to our dictionary
+                    badIPsAndReasons.Add(new KeyValuePair<string, string>(badIP, reason));
+                }
+
+                xmlReader.Close();
+                i++;
+            }
+
+            // Add html <p> elements to the right hand side literal (IP / DNS name / Reason)
+            output2.Text = $"<h2 id=\"numFound\">Found {badIPsAndReasons.Count} fraudulent IP addresses!</h2>";
+            output2.Text += "<h3><u>IP ID / IP address / IP's DNS host name / Reason IP is fraudulent</u>:</h3>";
+            output2.Text += "<h6>(DKIM stands for DomainKeys Identified Mail Protocol, SPF stands for Sender Policy Framework)<h6><br>";
+            int id = 1;
+            foreach (KeyValuePair<string, string> pair in badIPsAndReasons)
+            {
+                string ipStr = pair.Key;
+                string reason = pair.Value;
+
+                output2.Text += $"<p>{id} / {ipStr} / ";
+                try
+                {
+                    IPAddress ip = IPAddress.Parse(ipStr);
+                    IPHostEntry DNSname = Dns.GetHostEntry(ip);
+                    output2.Text += $"{DNSname.HostName} / ";
+                }
+                catch (System.Net.Sockets.SocketException)
+                {
+                    output2.Text += "Could not resolve this address. / ";
+                }
+
+                // No matter what happens, the reason for fraud is displayed and id is incremented
+                finally
+                {
+                    output2.Text += $"{reason}</p><br>";
+                    id++;
+                }
+            }
+
+            literal2.Visible = true;
+        }
+    }
+}
